1bc6a3ee71b03333702d6680504fddd4b6233593
limei <lmliang11@foxmail.com>
2024-10-19 21:01:40 +0800

    列表渲染

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+// 项目跟组件
+
+// App -> index.js  -> public/index.html(root)
+const count = 1
+
+const list = [
+  {id: 1001, name: 'limei'},
+  {id: 1002, name: 'haohui'},
+  {id: 1003, name: 'wenwu'},
+  {id: 1004, name: 'qingfeng'}
+]
+
+function getName(){
+  return "neko"
+}
 
 function App() {
   return (
     <div className="App">
       This is app
+      {/* 使用引号传递字符串 */}
+      {"this is a string"}
+
+      {/* js */}
+      {count}
+
+    {/* 函数调用 */}
+    {getName()}
+
+    {/* 方法调用 */}
+    {new Date().getDate()}
+
+    {/* 使用js对象 */}
+    <div style={{color:'red'}}>hello world</div>
+    
+    {/* 列表渲染,key要绑定 */}
+    <ul>
+      {list.map(item=><li key={item.id}>{item.name}</li>)}
+    </ul>
     </div>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
+// 把App根组件渲染到id为root的dom节点上 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
