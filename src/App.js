73068f4dd327e39ad850ed58d213d11f03b20f2b
limei <lmliang11@foxmail.com>
2024-10-20 13:01:39 +0800

    *

--- src/App.js
+++ src/App.js
@@ -10,8 +10,25 @@ const list = [
   {id: 1004, name: 'qingfeng'}
 ]
 
+const flag = true
+
+const type = 1 // 0 1 2
 function getName(){
   return "neko"
+}
+
+function getType(){
+  if (type === 0){
+    return <div style={{color:'pink'}}>我是typy1</div>
+  } else if (type === 1){
+    return <div style={{color:'blue'}}>我是type2</div>
+  }else{
+    return <div>我是type3</div>
+  }
+}
+
+const handleClick = () =>{
+  console.log("button 被点击了")
 }
 
 function App() {
@@ -37,8 +54,20 @@ function App() {
     <ul>
       {list.map(item=><li key={item.id}>{item.name}</li>)}
     </ul>
+
+    {/* 逻辑表达 */}
+    {flag && <span>true</span>}
+    {flag && <span>true</span>}
+
+    {flag ? <div>It is true</div> : <div>load...</div>}
+
+    {/* 复杂样式渲染 */}
+    {getType()}
+
+    {/* 绑定点击事件 */}
+    <button onClick={handleClick}>click me</button>
+
     </div>
-    
   );
 }
 
